6c0625043c3bc8ef2f12493a8fb46fb9de237488
FCI <fci@ADMINs-MacBook-Air.local>
2025-08-11 22:41:50 +0530

    fixed firebase id

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCX3gBQaT-a8y1Ma-bG6Q_vPVYpc3zxfSY",
-  authDomain: "paintikaa-1.firebaseapp.com",
-  projectId: "paintikaa-1",
-  storageBucket: "paintikaa-1.firebasestorage.app",
-  messagingSenderId: "223056785201",
-  appId: "1:223056785201:web:ae6393084137963ebc0a10",
-  measurementId: "G-E71PED5TGX"
+  apiKey: "AIzaSyDZI-WHdhe3WJwzP1TYlW6UVbDQdliX7Fs",
+  authDomain: "paintikaa-2.firebaseapp.com",
+  projectId: "paintikaa-2",
+  storageBucket: "paintikaa-2.firebasestorage.app",
+  messagingSenderId: "983042027142",
+  appId: "1:983042027142:web:864f70d49b8d55f967eb07",
+  measurementId: "G-S2H41SSE5D"
 };
 
 // Initialize Firebase
